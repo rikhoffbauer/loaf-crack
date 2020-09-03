1683b46827cfb17bcbfa458a97ca101d1a409a3e
Rik Hoffbauer <62353999+rikhoffbauer@users.noreply.github.com>
2020-09-03 02:08:41 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Loaf crack
+
+> Patches Loaf to accept any license key.
+
+## Usage
+
+```shell script
+# if you haven't installed Loaf yet
+brew cask install loaf
+
+# download and apply the patch
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/rikhoffbauer/loaf-crack/master/patch.bash)"
+```
+
+Now Loaf will accept any license key you enter.
---
+++ patch.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+TMP_DIR="${TMPDIR}loaf"
+LOAF_APP_ASAR_PATH="/Applications/Loaf.app/Contents/Resources/app.asar"
+LOAF_MAIN_JS="$TMP_DIR/main.prod.js"
+
+rm -rf "$TMP_DIR"
+mkdir "$TMP_DIR"
+
+npx asar e "$LOAF_APP_ASAR_PATH" "$TMP_DIR"
+
+sed -Ei 's/checkout\.getmotion\.io\//raw\.githubusercontent\.com\/rikhoffbauer\/loaf-license-server\/master\/response\.json#/g' "$LOAF_MAIN_JS"
+
+rm -f "${LOAF_APP_ASAR_PATH}.bak"
+mv "$LOAF_APP_ASAR_PATH" "${LOAF_APP_ASAR_PATH}.bak"
+
+npx asar p "$TMP_DIR" "$LOAF_APP_ASAR_PATH"
